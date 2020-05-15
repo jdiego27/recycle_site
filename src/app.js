c605e2f5fbbde2647e412fcfe026c3ad33547fff
JosymarM <josymar.mayhua@gmail.com>
2020-05-15 16:24:21 -0500

    New tab16

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(publicDir));
-app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/public/images'));
 
 
 
